b360b2cd1907cdb7a5a3ca2a5334c95cbba257e3
teresalazar13 <teresa.sal13@gmail.com>
2017-07-14 16:16:52 +0100

    Adds Analytics snippet in html.

--- app.js
+++ app.js
@@ -3,8 +3,3 @@ var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(process.env.PORT || 8080, function(){
   console.log('Server running on 8080...');
 });
-
-require('dotenv').config();
-var ua = require('universal-analytics');
-var visitor = ua(process.env.CONTROL_ID);
-visitor.pageview("/").send();
